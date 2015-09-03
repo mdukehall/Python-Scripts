14ded177c0051ac3f2f4acad37981a6d90ee9dc0
Duke Hall <duke.hall@gettyimages.com>
2015-09-03 10:46:19 -0700

    updates

--- client_tcp.py
+++ client_tcp.py
@@ -1,20 +1,20 @@
 import socket
 
-target_host="www.google.com"    #string DNS or IP
+target_host="michaeldukehall.com"    #string DNS or IP
 target_port=80                  #integer
 
 #create TCP socket
 #doc: https://docs.python.org/2/library/socket.html
 #AF_INET is for Address Family; ipv4; AF_INET6 would be ipv6, etc
 #SOCK_STREAM is default value, SOCK_DGRAM is for a data gram
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connect socket
 #doc: https://docs.python.org/2/library/socket.html?highlight=connect#socket.socket.connect
 #send data to socket
 #doc: https://docs.python.org/2/howto/sockets.html#socket-howto
 client.connect((target_host,target_port))
-    client.send("GET/HTTP/1.1\r\nHost:google.com\r\n\r\n")
+client.send("GET / HTTP/1.0\r\n\r\n")
 
 #receive data from socket
 #parameter is buffer size; should be multiple of 2 and small.
--- server_tcp.py
+++ server_tcp.py
@@ -13,7 +13,7 @@ server.bind((bind_ip,bind_port))
 #set max backlog to 5
 server.listen(5)
 
-print "[*] listing on %s:%d" % (bind_ip, bind_port)
+print "[*] listening on %s:%d" % (bind_ip, bind_port)
 
 #thread: manage client
 #perform a receive and respond.
@@ -23,9 +23,20 @@ def handle_client(client_socket):
     client_socket.send("message received captain")
     client_socket.close()
 
+#begin infinite loop; "break" is how to break this in Python
 while True:
+    #accept a socket
+    #doc: https://docs.python.org/2/library/socket.html
+    #   return value is a pair (conn, address)
+    #   conn is a new socket object usable to send and receive data on the connection
     client,addr = server.accept()
     print "[*] accepted connection from: %s:%d" % (addr[0],addr[1])
 
+    #create a thread.
+    #doc: https://docs.python.org/2/library/threading.html
     client_handler = threading.Thread(target=handle_client,args=(client,))
+    #start the thread; can only be done once/thread.  Otherwise raises RuntimeError
     client_handler.start()
+
+#TO TEST
+#run script; then browse to 0.0.0.0:9999.  result should be "message received captain"
